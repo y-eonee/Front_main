07458bdd2049c05f9ef8298d9e9baadddc17e2e1
yeonee <155801470+y-eonee@users.noreply.github.com>
2024-08-07 01:36:34 +0900

    particle star

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import React from 'react';
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls } from '@react-three/drei';
 import Main from './Main';
+import Particle from './Particle';
 
 function App() {
   return (
-    <Canvas>
-      <OrbitControls />
-      <Main />
-    </Canvas>
+    <div style={{ position: 'relative', width: '100vw', height: '100vh' }}>
+      <Particle />
+      <Canvas style={{ position: 'relative', zIndex: 0 }}>
+        <OrbitControls />
+        <Main />
+      </Canvas>
+    </div>
   );
 }
 
