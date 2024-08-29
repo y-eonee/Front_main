70979807946bda1d66695cf03d0f592b9aa916b6
yeonee <leenayeon@sookmyung.ac.kr>
2024-08-29 12:51:27 +0900

    리디렉션 수정

--- src/GoogleLogin.js
+++ src/GoogleLogin.js
@@ -1,27 +1,25 @@
 import { useGoogleLogin } from "@react-oauth/google";
 import React from "react";
 import axios from "axios";
-import { useNavigate } from "react-router-dom"; // react-router-dom 추가
 import styles from "./Login.module.css";
 
 export default function GoogleLogin() {
-  const navigate = useNavigate(); // useNavigate 훅 사용
-
   const handleGoogleLogin = useGoogleLogin({
     scope: "email profile",
     onSuccess: async ({ code }) => {
       try {
-        const { data } = await axios.post("http://localhost:8080/login/oauth2/code/google", { code });
-        console.log(data);
-
-        // 로그인 성공 후 리디렉션
-        navigate("/InGame"); // 또는 로그인 성공 후 리디렉션할 페이지
+        const response = await axios.post("http://localhost:8080/login/oauth2/code/google", { code });
+        // Assuming the server response includes a redirection URL
+        const redirectUrl = response.data.redirectUrl;
+        if (redirectUrl) {
+          window.location.href = redirectUrl;
+        }
       } catch (error) {
-        console.error("로그인 오류:", error);
+        console.error("Google login error:", error);
       }
     },
     onError: (errorResponse) => {
-      console.error("구글 로그인 오류:", errorResponse);
+      console.error("Google login error response:", errorResponse);
     },
     flow: "auth-code",
   });
